b0d2538762107701b8760023861a67a17ee79d67
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-01-28 04:54:36 +0300

    completed stage 1 of section 1

---
+++ src/main/java/ru/alternation/Simple/section1/stage1/step14/Main.java
@@ -0,0 +1,12 @@
+package ru.alternation.Simple.section1.stage1.step14;
+
+/**
+ Напишите программу, выводящую в консоль It's alive! It's alive!
+
+ Класс должен называться "Main", это ограничение проверяющей системы.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("It's alive! It's alive!");
+    }
+}
