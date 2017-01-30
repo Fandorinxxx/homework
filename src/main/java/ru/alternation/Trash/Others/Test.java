d4eb5e8b8cc4d7808a66eeffdcb5f60277c525e4
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-01-30 04:45:09 +0300

    add section2.stage1.step8

--- src/main/java/ru/alternation/Trash/Others/Test.java
+++ src/main/java/ru/alternation/Trash/Others/Test.java
@@ -7,6 +7,21 @@ public class Test {
     public static void main(String[] args) {
 
 
+
+//        long i = 100_000_000_000L;
+//        System.out.println(i++);
+//        i = 100_000_000_000L;
+//        System.out.println(--i);
+//
+//        byte b = -128;
+//        System.out.println((byte)(b-1));
+
+//        char c = 64;
+//        System.out.println(c);
+
+
+
+
     }
 
 
