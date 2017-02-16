b435e3a9af0b08e0d8ec1b09b23185c73fd1e117
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-02-17 02:27:07 +0300

    section2.stage2.step8 done

--- src/main/java/ru/alternation/Simple/section2/stage2/step8/Main.java
+++ src/main/java/ru/alternation/Simple/section2/stage2/step8/Main.java
@@ -34,11 +34,14 @@ public class Main {
      * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
      */
     public static boolean isPowerOfTwo(int value) {
-        return true; // you implementation here
+
+        return Integer.bitCount(Math.abs(value)) == 1;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(isPowerOfTwo(0));
+        System.out.println(isPowerOfTwo(1));
+        System.out.println(isPowerOfTwo(-2));
     }
 
 
