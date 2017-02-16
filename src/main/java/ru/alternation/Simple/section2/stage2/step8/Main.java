cec60522c2c3187393eb5cade778fec8e0803ad6
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-02-17 02:23:21 +0300

    add section2.stage2.step8 class

---
+++ src/main/java/ru/alternation/Simple/section2/stage2/step8/Main.java
@@ -0,0 +1,45 @@
+package ru.alternation.Simple.section2.stage2.step8;
+
+/**
+ Реализуйте метод, проверяющий, является ли заданное число по абсолютной величине степенью двойки.
+
+ Решать можно разными способами:
+
+ воспользовавшись одним удобным статическим методом из класса java.lang.Integer;
+ применив пару трюков из двоичной арифметики;
+ написав решение "в лоб" с циклом и условными операторами (можете вернуться к этой задаче после просмотра соответствующих уроков).
+
+ Sample Input 1:
+ 0
+ Sample Output 1:
+ false
+
+ Sample Input 2:
+ 1
+ Sample Output 2:
+ true
+
+ Sample Input 3:
+ -2
+ Sample Output 3:
+ true
+
+ */
+public class Main {
+
+    /**
+     * Checks if given <code>value</code> is a power of two.
+     *
+     * @param value any number
+     * @return <code>true</code> when <code>value</code> is power of two, <code>false</code> otherwise
+     */
+    public static boolean isPowerOfTwo(int value) {
+        return true; // you implementation here
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/main/java/ru/alternation/Trash/Others/Test.java
+++ src/main/java/ru/alternation/Trash/Others/Test.java
@@ -11,43 +11,6 @@ public class Test {
 
 
 
-        int i;
-
-        i=101;
-        System.out.println(Integer.toBinaryString( i));
-        i=127;
-        System.out.println(Integer.toBinaryString( i));
-
-
-        i<<=32;
-        System.out.println(Integer.toBinaryString( i));
-        System.out.println(i);
-
-        i>>=32;
-        System.out.println(Integer.toBinaryString( i));
-        System.out.println(i);
-
-        i>>>=5;
-        System.out.println(Integer.toBinaryString( i));
-        System.out.println(i);
-
-
-
-
-
-
-
-//        byte b = -128;
-//        System.out.println(Integer.toBinaryString( b));
-//        System.out.println((byte)(--b));
-//        System.out.println(Integer.toBinaryString(b));
-
-
-
-
-
-
-
     }
 
 
