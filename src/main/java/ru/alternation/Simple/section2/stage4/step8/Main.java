47fbd3b23ef7ebc65ca77db9b4fcccd4fc496ee9
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-02-19 19:47:23 +0300

    add s2s4 classes

---
+++ src/main/java/ru/alternation/Simple/section2/stage4/step8/Main.java
@@ -0,0 +1,44 @@
+package ru.alternation.Simple.section2.stage4.step8;
+
+import java.math.BigInteger;
+
+/**
+ Реализуйте метод, вычисляющий факториал заданного натурального числа.
+
+ Факториал NN вычисляется как 1⋅2⋅...⋅N1⋅2⋅...⋅N.
+
+ Поскольку это очень быстро растущая функция, то даже для небольших NN вместимости типов int и long очень скоро не хватит.
+ Поэтому будем использовать BigInteger.
+
+ Воспользуйтесь предоставленным шаблоном. Декларацию класса, метод main и обработку ввода-вывода добавит проверяющая система.
+
+ Sample Input 1:
+ 1
+ Sample Output 1:
+ 1
+
+ Sample Input 2:
+ 3
+ Sample Output 2:
+ 6
+
+ */
+
+public class Main {
+
+    /**
+     * Calculates factorial of given <code>value</code>.
+     *
+     * @param value positive number
+     * @return factorial of <code>value</code>
+     */
+    public static BigInteger factorial(int value) {
+
+
+        return new BigInteger("0"); // your implementation here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
