b8350058136bc31a97f56557271bd653eccdddba
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-04-03 05:34:11 +0300

    section4 initial

---
+++ src/main/java/ru/alternation/Simple/section4/stage1/step9/Main.java
@@ -0,0 +1,20 @@
+package ru.alternation.Simple.section4.stage1.step9;
+
+/**
+ * Created by Alternation on 03.04.2017.
+ *
+ * Реализуйте метод sqrt(), вычисляющий квадратный корень числа.
+ * В отличие от Math.sqrt(), это метод при передаче отрицательного параметра должен бросать
+ * исключение java.lang.IllegalArgumentException с сообщением "Expected non-negative number, got ?",
+ * где вместо вопросика будет подставлено фактически переданное в метод число.
+ */
+public class Main {
+
+    public static double sqrt(double x) {
+        return 0; // your implementation here
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
