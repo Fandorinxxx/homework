d346b084765a757e074b7f44e7764069a9be7d40
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-04-11 02:51:04 +0300

    add initial conditions for sc5st3cp12-13

--- src/main/java/ru/alternation/Simple/section5/stage3/step12/Main.java
+++ src/main/java/ru/alternation/Simple/section5/stage3/step12/Main.java
@@ -1,24 +1,32 @@
 package ru.alternation.Simple.section5.stage3.step12;
 
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
 
 /**
+ Реализуйте метод, который зачитает данные из InputStream и преобразует их в строку, используя заданную кодировку.
+
+ Пример:
+ InputStream последовательно возвращает четыре байта: 48 49 50 51.
+
+ Метод, вызванный для такого InputStream и кодировки ASCII, должен вернуть строку "0123".
+
+
  https://stepik.org/media/attachments/lesson/12782/module5.pdf
  */
 public class Main {
 
+
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        // your implementation here
+        return null;
+    }
+
     public static void main(String[] args) {
 
 
-        PrintWriter printWriter = new PrintWriter(new OutputStreamWriter(System.out, StandardCharsets.US_ASCII));
-        printWriter.write("Ы");
-        printWriter.flush();
-
 
     }
-
-
 }
