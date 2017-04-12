f2935d1930874a6ef59dbc9159ce70e08e1bea5a
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-04-13 00:21:53 +0300

    section5.stage4.step8 done+

--- src/main/java/ru/alternation/Simple/section5/stage4/step8/Main.java
+++ src/main/java/ru/alternation/Simple/section5/stage4/step8/Main.java
@@ -2,8 +2,10 @@ package ru.alternation.Simple.section5.stage4.step8;
 
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -29,30 +31,62 @@ import java.util.Objects;
  https://stepik.org/media/attachments/lesson/12782/module5.pdf
  */
 public class Main {
-    public static Animal[] deserializeAnimalArray(byte[] data) {
 
-        Animal[] animals;
+    public static Animal[] deserializeAnimalArray(byte[] data) {
         try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data))) { // IOException
             int count = ois.readInt(); // IOException
-            animals = new Animal[count];
+            Animal[] animals = new Animal[count];
             for (int i = 0; i < count; i++) {
-                Object o = ois.readObject(); // IOException
-                if (o instanceof Animal){
-                    animals[i] = (Animal)o; // ClassNotFoundException
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                animals[i] = (Animal)ois.readObject(); // IOException | ClassNotFoundException |
+                                                       // ClassCastException - т.к. без instanceOf
+//________________________________________________________________________________________________
+//                Object o = ois.readObject();  // IOException | ClassNotFoundException
+//                if (o instanceof Animal){     // ClassCastException не будет
+//                    animals[i] = (Animal)o;
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
             }
-        } catch (IOException | ClassNotFoundException e){
-            throw new IllegalArgumentException();
+            return animals;
+
+        } catch (IOException | ClassNotFoundException | ArrayIndexOutOfBoundsException | ClassCastException e){
+            throw new IllegalArgumentException(e);
         }
-        return animals;
     }
+
+
+
 
     public static void main(String[] args) {
+        test();
+    }
 
+    private static void test(){
+        int size = 5;
+        Animal[] animals = new Animal[size];
+        for (int i = 0; i < size; i++) {
+            animals[i] = new Animal("Animal " + (i+1));
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        try(ObjectOutputStream outputStream = new ObjectOutputStream(byteArrayOutputStream)) {
+            outputStream.writeInt(size);
+            for (Animal a : animals){
+                outputStream.writeObject(a);
+            }
+            outputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Animal[] animals2;
+        animals2 = deserializeAnimalArray(byteArrayOutputStream.toByteArray());
+        for (Animal animal : animals2){
+            System.out.println(animal);
+        }
     }
 }
+
 
 class Animal implements Serializable {
     private final String name;
@@ -67,5 +101,12 @@ class Animal implements Serializable {
             return Objects.equals(name, ((Animal) obj).name);
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "name='" + name + '\'' +
+                '}';
     }
 }
