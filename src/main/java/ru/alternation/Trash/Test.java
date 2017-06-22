9c772abff1e81d1af4e9da236cbdb5dd245be191
Alternation <16310547+AlternatiOne@users.noreply.github.com>
2017-06-22 20:11:48 +0300

    add additional examples of func stepik lessons

--- src/main/java/ru/alternation/Trash/Test.java
+++ src/main/java/ru/alternation/Trash/Test.java
@@ -1,15 +1,11 @@
 package ru.alternation.Trash;
 
 
-import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
 
 
-        Arrays.stream(new int[]{5,7}).min();
-
     }
-
 
 }
